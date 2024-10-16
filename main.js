7b42552a005cb06a267454b8d05bf9bfc191fae1
OnyxDev87 <mileswilander@icloud.com>
2024-10-16 17:10:45 +0000

    pls work

--- main.js
+++ main.js
@@ -6,31 +6,31 @@ canvas.height = 630
 
 let counter = 0
 let trialList = []
-let Xoffset = 10
-let Yoffset = 0
+let Xoffset = [10]
+let Yoffset = [0]
 
-alert("Press space to add to the counter, press 'r' to reset it and save the value, press 'p' to clear all saved data")
-alert("I MADE CHANGES AND IT IS A LITTLE GLITCHY RIGHT NOW/n-Miles")
+// alert("Press space to add to the counter, press 'r' to reset it and save the value, press 'p' to clear all saved data")
+// alert("I MADE CHANGES AND IT IS A LITTLE GLITCHY RIGHT NOW-Miles")
 
 function animate() {
-    window.requestAnimationFrame(animate)
+  window.requestAnimationFrame(animate)
 
-    c.fillStyle = '#1e1e1e'
-    c.fillRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = '#1e1e1e'
+  c.fillRect(0, 0, canvas.width, canvas.height)
 
-    c.fillStyle = '#d3d3d3'
-    c.font = '75px Arial'
-    c.fillText('Counter: ' + counter, 0, 60)
-    c.font = '25px Arial'
-    c.fillText("Previous Trials:", 0, 100)
-    for (let i = 0; i < trialList.length; i++) {
-      c.fillText(JSON.stringify(trialList[i]), Xoffset, 125+(i*25))
+  c.fillStyle = '#d3d3d3'
+  c.font = '75px Arial'
+  c.fillText('Counter: ' + counter, 0, 60)
+  c.font = '25px Arial'
+  c.fillText("Previous Trials:", 0, 100)
+  for (let i = 0; i < trialList.length; i++) {
+    c.fillText(JSON.stringify(trialList[i]), Xoffset[i], 125+(Yoffset[i]*25))
 
-      if (125+(Yoffset*25) >= canvas.height) {
-        Yoffset = 0
-        Xoffset += 300
-      }
+    if (125+(Yoffset[i]*25) >= canvas.height) {
+      Yoffset.push(0)
+      Xoffset.push(300)
     }
+  }
 }
 
 document.addEventListener("keydown", (event) => {
